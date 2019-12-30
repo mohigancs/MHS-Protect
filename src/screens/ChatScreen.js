e3da2248d8963f80a968b5c4e3e37b529286b55a
Craig <mcraigdombrowski@gmail.com>
2019-12-30 09:23:38 -0500

    Fixed Gifted Chat keyboard padding

--- src/screens/ChatScreen.js
+++ src/screens/ChatScreen.js
@@ -1,10 +1,11 @@
-import { GiftedChat } from 'react-native-gifted-chat'
-import React, { Component } from 'react'
-import {Appbar, IconButton} from 'react-native-paper'
-import { KeyboardAvoidingView, View } from 'react-native'
-import Database from './components/Database'
+import { GiftedChat } from 'react-native-gifted-chat';
+import React, { Component } from 'react';
+import {Appbar, IconButton} from 'react-native-paper';
+import { KeyboardAvoidingView, View } from 'react-native';
+import { Platform } from '@unimodules/core';
+import Database from './components/Database';
 
-const db = new Database()
+const db = new Database();
 
 export default class ChatScreen extends Component {
 
@@ -31,27 +32,51 @@ export default class ChatScreen extends Component {
     }    
 
     render() {
-        return (     
-            <View style = {{flex:1}}>
-                <Appbar.Header
+        if (Platform.OS === 'ios') {
+            return (     
+                <View style={{flex:1}}>
+                    <Appbar.Header
                     dark = {true}
                     style={{backgroundColor:'#bfbfbf'}}
-                >
-                    <Appbar.Action
-                    icon = 'arrow-left'
-                    size = {24}
-                    color = '#ffffff'
-                    onPress={() => this.props.navigation.navigate('Home')} />
+                    >
+                        <Appbar.Action
+                        icon = 'arrow-left'
+                        size = {24}
+                        color = '#ffffff'
+                        onPress={() => this.props.navigation.navigate('Home')} />
                 </Appbar.Header>
-                <GiftedChat
+
+                    <GiftedChat
                         messages={this.state.messages}
-                        //onSubmitEditing={db.send} this don't work and idk why hnghh
                         onSend={db.send}
                         user={this.user}
-                />
-                <KeyboardAvoidingView behavior={'padding'}/> 
-            </View>
-        )
+                    />
+                    <KeyboardAvoidingView behavior={'position'}/> 
+                </View>
+            )
+        }
+        else {
+            return (     
+                <View style={{flex:1}}>
+                    <Appbar.Header
+                    dark = {true}
+                    style={{backgroundColor:'#bfbfbf'}}
+                    >
+                        <Appbar.Action
+                        icon = 'arrow-left'
+                        size = {24}
+                        color = '#ffffff'
+                        onPress={() => this.props.navigation.navigate('Home')} />
+                </Appbar.Header>
+                    <GiftedChat
+                        messages={this.state.messages}
+                        onSend={db.send}
+                        user={this.user}
+                    />
+                    <KeyboardAvoidingView behavior={'padding'}/> 
+                </View>
+            )
+        }
     }
 
     componentWillUnmount() {
