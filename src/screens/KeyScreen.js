e9bf7a5db0567ffadd52a70ee76324344465bf91
Geoffrey Swisher <gdswish2@stu.k12.wv.us>
2020-01-30 13:22:45 -0500

    padding fix

--- src/screens/KeyScreen.js
+++ src/screens/KeyScreen.js
@@ -32,24 +32,41 @@ export default class KeyScreen extends React.Component {
         const {assetsLoaded} = this.state
         if( assetsLoaded ) {
             return (
-                <View style={styles.container}>
-                    <Image
-                        style={styles.image}
-                        source={require('../images/logo.png')} 
-                    />
-                    <Text style={styles.title}>Enter Your Key Below</Text>
-                    <TextInput
-                        style={styles.input}
-                        placeholder="Access Code"
-                        placeholderTextColor = "black"
-                        returnKeyType="go"
-                        autoCapitalize="none"
-                        autoCorrect={false}
-                        onChange={(text) => this.key = text.nativeEvent.text}
-                        
-                        onSubmitEditing={() => {
-                            console.log(screenWidth)
-                            console.log(screenHeight)
+                <KeyboardAvoidingView style={styles.container} behavior="padding" enabled>
+                    <View style={styles.container}>
+                        <Image
+                            style={styles.image}
+                            source={require('../images/logo.png')} 
+                        />
+                        <Text style={styles.title}>Enter Your Key Below</Text>
+                        <TextInput
+                            style={styles.input}
+                            placeholder="Access Code"
+                            placeholderTextColor = "black"
+                            returnKeyType="go"
+                            autoCapitalize="none"
+                            autoCorrect={false}
+                            onChange={(text) => this.key = text.nativeEvent.text}
+                            
+                            onSubmitEditing={() => {
+                                console.log(screenWidth)
+                                console.log(screenHeight)
+                                db.isValidKey(this.key).then(result => {
+                                    if (result[0]) {
+                                        //Alert.alert('Success!', 'Your key was found in the database.')
+                                        db.fetchUser(result[1]).then(user => {
+                                            this.props.navigation.navigate('Confirm', {user: [user, result[1]]})
+                                        })
+                                        //db.removekey(result[1], false)
+                                    } else {
+                                        Alert.alert('Key Not Found.', 'Visit Mr. Gibson for help.')
+                                    }
+                                })
+                            }}
+                        />
+                        <TouchableOpacity 
+                        style={styles.button}
+                        onPress={() => {
                             db.isValidKey(this.key).then(result => {
                                 if (result[0]) {
                                     //Alert.alert('Success!', 'Your key was found in the database.')
@@ -62,26 +79,11 @@ export default class KeyScreen extends React.Component {
                                 }
                             })
                         }}
-                    />
-                    <TouchableOpacity 
-                    style={styles.button}
-                    onPress={() => {
-                        db.isValidKey(this.key).then(result => {
-                            if (result[0]) {
-                                //Alert.alert('Success!', 'Your key was found in the database.')
-                                db.fetchUser(result[1]).then(user => {
-                                    this.props.navigation.navigate('Confirm', {user: [user, result[1]]})
-                                })
-                                //db.removekey(result[1], false)
-                            } else {
-                                Alert.alert('Key Not Found.', 'Visit Mr. Gibson for help.')
-                            }
-                        })
-                    }}
-                    >
-                        <Text style = {styles.text}>SUBMIT</Text>
-                    </TouchableOpacity>
-                </View>
+                        >
+                            <Text style = {styles.text}>SUBMIT</Text>
+                        </TouchableOpacity>
+                    </View>
+                </KeyboardAvoidingView>
             )
         } else {
             return (
